803a156d5cf70f3ea5fdba7b8bdff91da5cb7d55
Masayuki Sunahara <tamanishi@gmail.com>
2021-12-18 13:43:57 +0900

    format

--- network.tf
+++ network.tf
@@ -151,23 +151,23 @@ resource "aws_route_table" "private_1" {
 }
 
 resource "aws_route" "private_0" {
-  route_table_id = aws_route_table.private_0.id
-  nat_gateway_id = aws_nat_gateway.nat_gateway_0.id
+  route_table_id         = aws_route_table.private_0.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_0.id
   destination_cidr_block = "0.0.0.0/0"
 }
 
 resource "aws_route" "private_1" {
-  route_table_id = aws_route_table.private_1.id
-  nat_gateway_id = aws_nat_gateway.nat_gateway_1.id
+  route_table_id         = aws_route_table.private_1.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_1.id
   destination_cidr_block = "0.0.0.0/0"
 }
 
 resource "aws_route_table_association" "private_0" {
-  subnet_id = aws_subnet.private_0.id
-  route_table_id=aws_route.private_0.id
+  subnet_id      = aws_subnet.private_0.id
+  route_table_id = aws_route.private_0.id
 }
 
 resource "aws_route_table_association" "private_1" {
-  subnet_id = aws_subnet.private_1.id
-  route_table_id=aws_route.private_1.id
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route.private_1.id
 }
